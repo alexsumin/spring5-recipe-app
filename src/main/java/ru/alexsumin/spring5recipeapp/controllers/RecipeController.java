718a1f38649a4f6adb16a6de1935a1336b27d9b6
alexsumin <alexandr.sumin@gmail.com>
2018-05-24 01:31:03 +0300

    spring mvc exception handler

--- src/main/java/ru/alexsumin/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/ru/alexsumin/spring5recipeapp/controllers/RecipeController.java
@@ -1,13 +1,13 @@
 package ru.alexsumin.spring5recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import ru.alexsumin.spring5recipeapp.commands.RecipeCommand;
+import ru.alexsumin.spring5recipeapp.exceptions.NotFoundException;
 import ru.alexsumin.spring5recipeapp.services.RecipeService;
 
 @Slf4j
@@ -56,5 +56,17 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
-}
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
+    }
+}
